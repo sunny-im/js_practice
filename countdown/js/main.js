9209cc2f024a4697dd1a6e056dbe69ec1a7fac1f
sunny-im <hellossun.dahae@gmail.com>
2022-04-17 22:44:33 +0900

    Dday ver.2

--- countdown/js/main.js
+++ countdown/js/main.js
@@ -1,11 +1,15 @@
 function bDay() {
     var dDay = document.getElementById("myBday");
     var today = new Date();
-    var birth = new Date(2022,09,25);
+    var birth = new Date("october 25,2022 0:00:00");
     var remain = birth.getTime() - today.getTime();
     var day = Math.ceil(remain/(1000*60*60*24));
+    var hour = Math.ceil(remain%(1000*60*60*24) / (1000*60*60));
+    var min = Math.ceil(remain%(1000*60*60) / (1000*60));
+    var sec = Math.ceil(remain%(1000*60) / 1000);
+    
 
-    dDay.innerHTML = (day + "일" + " 남았습니다 두둥 !!");
+    dDay.innerHTML = (day + "일 " + hour + "시간 " + min + "분 " + sec + "초 " + " 남았습니다 두둥 !!");
 }
 
 bDay();
