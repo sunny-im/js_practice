0d0a806fb02b3feab4fbe741586487683c7dbdfc
sunny-im <hellossun.dahae@gmail.com>
2022-04-19 01:35:03 +0900

    0418 js record

--- countdown/js/main.js
+++ countdown/js/main.js
@@ -1,16 +1,32 @@
-function bDay() {
-    var dDay = document.getElementById("myBday");
-    var today = new Date();
-    var birth = new Date("october 25,2022 0:00:00");
-    var remain = birth.getTime() - today.getTime();
-    var day = Math.ceil(remain/(1000*60*60*24));
-    var hour = Math.ceil(remain%(1000*60*60*24) / (1000*60*60));
-    var min = Math.ceil(remain%(1000*60*60) / (1000*60));
-    var sec = Math.ceil(remain%(1000*60) / 1000);
-    
+const dDay = document.getElementById("myBday");
+const record_ul = document.getElementById("record_ul");
 
-    dDay.innerHTML = (day + "일 " + hour + "시간 " + min + "분 " + sec + "초 " + " 남았습니다." +"<br>"+"두둥 !!");
+
+function bDay() {
+    let today = new Date();
+    let birth = new Date("october 25,2022 0:00:00");
+    let remain = birth.getTime() - today.getTime();
+    let day = Math.ceil(remain/(1000*60*60*24));
+    let hour = Math.ceil(remain%(1000*60*60*24) / (1000*60*60));
+    let min = Math.ceil(remain%(1000*60*60) / (1000*60));
+    let sec = Math.ceil(remain%(1000*60) / 1000);
+    
+    let result = day + "일 " + hour + "시간 " + min + "분 " + sec + "초 " + " 남았습니다." +"<br>"+" "
+    dDay.innerHTML = result;
 }
 
 bDay();
 setInterval(bDay,1000);
+
+let click = document.getElementById("click");
+click.addEventListener('click', record);
+
+function record() {
+    const record_li = document.createElement("li");
+    record_li.innerHTML = dDay.innerHTML;
+    
+    // record_ul.appendChild(record_li); //새로운 노드를 해당 노드의 자식 노드 리스트의 맨 마지막에 추가(태그를 만들고 태그를 추가 할 때 사용)
+    record_ul.append(record_li); // record_ul(부모)태그 가장 뒤에 record_li
+    record_ul.prepend(record_li); // record_ul(부모)태그 가장 앞에 record_li
+}
+
--- realTime/js/main.js
+++ realTime/js/main.js
@@ -10,8 +10,10 @@ function getTime(){
 }
 
 function convertTime(time){
-    return time < 10 ? `0${time}` : time
+    return time < 10 ? `0${time}` : time    // 리터럴함수
 }
 
 getTime();
 setInterval(getTime,1000);
+// setInterval : 주기적으로 인자를 실행하는 함수
+// setTimeout : 일정시간이 지난 후 인자로 받은 함수를 실행
